acb3e9995e9efd5560c483484e5392b346adca5b
bonabanquet <mynjy94@gmail.com>
2021-07-09 21:19:39 +0900

    CommentDTO 외 추가

---
+++ hnmk/src/main/java/kr/co/jhta/cinema/dao/CommentDAOImple.java
@@ -0,0 +1,41 @@
+package kr.co.jhta.cinema.dao;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import kr.co.jhta.cinema.dto.CommentDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+@Repository
+@Getter
+@Setter
+public class CommentDAOImple implements CommentDAO {
+	@Autowired
+	private SqlSession ss;
+	
+	@Override
+	public List<CommentDTO> readAll() {
+		return ss.selectList("kr.co.jhta.cinema.commentAll");
+	}
+
+	@Override
+	public CommentDTO readOne(int mno, int customerno) {
+		Map<String, Integer> parameters = new HashMap<String, Integer>();
+		parameters.put("mno",  mno);
+		parameters.put("customerno", customerno);
+		return ss.selectOne("kr.co.jhta.cinema.commentOne", parameters);
+	}
+
+	@Override
+	public void insertOne(CommentDTO dto) {
+		
+		ss.insert("kr.co.jhta.cinema.cminsertOne", dto);
+	}
+	
+}
